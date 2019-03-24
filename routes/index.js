dd805863bac2c2796c28b432ef457922431de291
h-powerstone <manish.gupta.6038@gmail.com>
2019-03-24 11:50:17 +0530

    PassPortJS

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,16 @@ const { ensureAuthenticated } = require("../config/auth");
 
 // Welcome Page 
 router.get("/", (req , res)=>{
+    res.render("home");
+});
+// About Page
+router.get("/about" , (req , res)=>{
+    res.render("about");
+})
+// Sign_in Page
+router.get("/users/", (req , res)=>{
     res.render("welcome");
 });
-
 // DashBoard Page
 router.get("/dashboard", ensureAuthenticated , (req , res)=>{
     res.render("dashboard" , {
